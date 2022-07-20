d49521615dc26f6d9ebbc0e068910affa690f53e
Ibtsam Khan <66584040+iibtsamkhan@users.noreply.github.com>
2022-07-20 07:59:03 +0500

    Update index.php

--- index.php
+++ index.php
@@ -21,10 +21,10 @@
       <div class="content">
         <div class="center" id="title">
           Hi, my name is<br />
-          <span class="emphasis" id="name">Leander Rodrigues</span>.
+          <span class="emphasis" id="name">Ibtsam Khan</span>.
           <div id="subtitle">
             I'm an undergrad
-            <span class="emphasis">engineer</span>, and an aspiring
+            <span class="emphasis">IT Student</span>, and an aspiring
             <span class="emphasis">programmer</span>.
           </div>
         </div>
@@ -38,13 +38,13 @@
         <div class="text" id="description-group">
           <!--SELF-CODE BLOCK-->
           <div class="round" id="self-object">
-            <pre>let <b>leander</b> = {</pre>
-            <pre>  <b>age</b>: 19, <span>//Soon to be 20!</span></pre>
-            <pre>  <b>school</b>: 'University of Waterloo',</pre>
-            <pre>  <b>program</b>: 'Nanotechnology Engineering',</pre>
-            <pre>  <b>favouriteLanguage</b>: 'JavaScript',</pre>
-            <pre>  <b>currentSchoolTerm</b>: 5, <span>//2B - second year, second term</span>  </pre>
-            <pre>  <b>awaitingWorkTerm</b>: 4, <span>//Completed three 4-month terms</span></pre>
+            <pre>let <b>Ibtsam</b> = {</pre>
+            <pre>  <b>age</b>: 19, <span>//Soon to be 22!</span></pre>
+            <pre>  <b>school</b>: 'Bahauddin Zakariya University, Multan',</pre>
+            <pre>  <b>program</b>: 'Information Technology',</pre>
+            <pre>  <b>favouriteLanguage</b>: 'JavaScript & php',</pre>
+            <pre>  <b>currentSemester</b>: 6, <span>//2B - 4th year, 7th semester</span>  </pre>
+            <pre>  <b>awaitingSemester</b>: 7, <span>//Completed 6 semesters</span></pre>
             <pre>  <b>hardWorker</b>: true,</pre>
             <pre>  <b>teamPlayer</b>: true,</pre>
             <pre>  <b>isPerfect</b>: false, <span>//Should never return true</span></pre>
@@ -52,7 +52,7 @@
           </div>
           <!--DESCRIPTION-->
           <p>
-          <code>Hello world</code>, my name is Leander Rodrigues and you've stumbled upon my landing page! Take some time to peruse my work and stay a while. So a little bit about me, I'm currently enrolled at the <em>University of Waterloo</em>, but I grew up in the suburbs of Mississauga, Ontario. I was accepted into this newfangled program, made only for the coolest of cats: <em>Nanotechnology Engineering</em>. It seemed like an obvious choice for someone like yours truly, since it sounds fancy, and I wanted to work with the latest and greatest in tech. As time goes by (I'm finishing up my second year!), it has definitely gone more towards the 'nano' and less towards the 'technology', so I figured now would be a good a time as any to pivot.</p><br />
+          <code>Hello world</code>, my name is Ibtsam Khan and you've stumbled upon my landing page! Take some time to peruse my work and stay a while. So a little bit about me, I'm currently enrolled at the <em>University of Waterloo</em>, but I grew up in the suburbs of Mississauga, Ontario. I was accepted into this newfangled program, made only for the coolest of cats: <em>Nanotechnology Engineering</em>. It seemed like an obvious choice for someone like yours truly, since it sounds fancy, and I wanted to work with the latest and greatest in tech. As time goes by (I'm finishing up my second year!), it has definitely gone more towards the 'nano' and less towards the 'technology', so I figured now would be a good a time as any to pivot.</p><br />
           <p>This site, and all its shiny buttons, animations, and links are a testament to my efforts to <em>break into the programmer scene</em>. My determination coupled with countless online resources has let me do some really cool stuff that I'd love for you (whoever you may be) to see (because it'll only grow my massive ego). While my education background is anything but <em> traditional</em>, I choose to embrace that weirdness, and redirect it into my work.</p>
           <br />
           <p>On the lighter side, I'm a pretty cool guy (if I do say so myself) outside of work. In my down time, you'll pretty much always catch me with headphones on. I'm either listening to tunez (mandatory letter  'z'), podcasts, or my friends scream at me in Discord. If you need someone to game with, someone to rip into bad movies with, or just someone to explain why dogs > cats, I'm definitely your guy. Aside from that, if I'm not going through a mountain of homework, working on a self-imposed project, or talking to/arguing with my friends online, It's pretty safe to say I'm asleep or dead.</p>
@@ -69,20 +69,11 @@
           <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
             <img class="round" src="/resources/images/about/javascript.png" alt="JavaScript Logo"/>
           </a>
-          <a href="https://reactjs.org/" target="_blank">
-            <img class="round" src="/resources/images/about/reactjs.png" alt="React.js Logo"/>
-          </a>
-          <!-- <a href="https://angularjs.org/" target="_blank">
-            <img class="round" src="/resources/images/about/angularjs.png" alt="Angular.js Logo"/>
-          </a> -->
           <a href="https://nodejs.org/en/" target="_blank">
             <img class="round" src="/resources/images/about/nodejs.png" alt="Node.js Logo"/>
           </a>
           <a href="https://git-scm.com/" target="_blank">
             <img class="round" src="/resources/images/about/git.png" alt="Git Logo"/>
-          </a>
-          <a href="https://aws.amazon.com/" target="_blank">
-            <img class="round" src="/resources/images/about/aws.png" alt="AWS Logo"/>
           </a>
           <a href="https://developer.mozilla.org/en-US/docs/Web/Guide/AJAX" target="_blank">
             <img class="round" src="/resources/images/about/ajax.png" alt="AJAX Logo"/>
@@ -156,7 +147,7 @@
             <h3>Are you hiring?</h3><br/>
             <p class="indent">In case you'd like to know a little more about me, check out my resume! It's a nice way to see all of my school/project experience and work history in one convenient place. As per your preference, I have both a direct PDF link and you can check out my hiring page for a web version and even more information!</p>
             <div>
-              <a class='far fa-file-alt' href='/resume/Leander Rodrigues - Resume.pdf' title=".pdf (0.14 MB)"></a>
+              <a class='far fa-file-alt' href='/resume/Resume-Ibtsam Ishtiaq.pdf' title=".pdf (0.14 MB)"></a>
               <a class='fas fa-link' href='/resume/index.php' title="Web Page"></a>
             </div>
             <h4>Thank you for your consideration!</h4>
